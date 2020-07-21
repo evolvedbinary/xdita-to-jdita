32f77bf7a95ad3999bc7d868beb1470e921b6e89
Charafeddine Cheraa <ccheraa@gmail.com>
2020-07-21 13:36:22 +0100

    sdded shortdesc node

--- src/lwdita.ts
+++ src/lwdita.ts
@@ -147,6 +147,9 @@ export interface IntReuse {
     id?: NMTOKEN;
     conref?: CDATA;
 }
+export const isIntReuse = (value?: any): value is IntReuse =>
+    isOrUndefined(isCDATA, value['id']) &&
+    isOrUndefined(isCDATA, value['conref']);
 
 export interface IntReferenceContent {
     href?: CDATA;
@@ -187,7 +190,7 @@ export interface IntTopic extends IntNamedElement, IntLocalization {
     //TODO(AR) how to ensure correct ordering of children here? also cannot have a shortdesc without a title! Need somelike like a HList
     // children: {
     //     title?: Title;
-    //     shortdesc?: Shortdesc;
+    //     shortdesc?: ShortDesc;
     //     prolog?: Prolog;
     //     body?: Body;
     // };
@@ -307,21 +310,70 @@ export class Title extends BaseElement implements IntTitle {
         return this.readProp<CDATA>('className'); }
 }
 
-// export interface IntShortdesc extends IntFilters, IntLocalization, IntReuse {
-//     outputClass?: CDATA;
-//     className?: CDATA;
-//     children: Array<AllInline>;
-// }
-// export class Shortdesc extends BaseElement implements IntShortdesc {
-//     readonly nodeName = 'shortdesc';
-//     children = [];
-//     constructor(
-//         public outputClass?: CDATA,
-//         public className?: CDATA,
-//     ) {
-//         super();
-//     }
-// }
+export interface IntShortDesc extends IntFilters, IntLocalization, IntReuse {
+    'props'?: CDATA;
+    'dir'?: CDATA;
+    'xml:lang'?: CDATA;
+    'translate'?: CDATA;
+    'id'?: NMTOKEN;
+    'conref'?: CDATA;
+    'outputClass'?: CDATA;
+    'className'?: CDATA;
+    // 'children': Array<AllInline>;
+}
+export const isIntShortDesc = (value?: any): value is IntShortDesc =>
+    isOrUndefined(isCDATA, value['outputClass']) &&
+    isOrUndefined(isCDATA, value['className']) &&
+    isIntLocalization(value) &&
+    isIntFilters(value) &&
+    isIntReuse(value);
+export class ShortDesc extends BaseElement implements IntShortDesc {
+    static nodeName = 'shortdesc';
+    static fields = [
+        'props',
+        'dir',
+        'xml:lang',
+        'translate',
+        'id',
+        'conref',
+        'outputClass',
+        'className',
+    ];
+    static childGroups = ['all-inline'];
+    constructor(attributes?: Attributes) {
+        super();
+        this._props = this.attributesToProps(attributes);
+    }
+    isValidField(field: string, value: any): boolean {
+        switch(field) {
+            case 'props': return isOrUndefined(isCDATA, value);
+            case 'dir': return isOrUndefined(isCDATA, value);
+            case 'xml:lang': return isOrUndefined(isCDATA, value);
+            case 'translate': return isOrUndefined(isCDATA, value);
+            case 'id': return isOrUndefined(isCDATA, value);
+            case 'conref': return isOrUndefined(isCDATA, value);
+            case 'outputClass': return isOrUndefined(isCDATA, value);
+            case 'className': return isOrUndefined(isCDATA, value);
+            default: return false;
+        }
+    }
+    get 'props'(): CDATA | undefined {
+        return this.readProp<CDATA>('props'); }
+    get 'dir'(): CDATA | undefined {
+        return this.readProp<CDATA>('dir'); }
+    get 'xml:lang'(): CDATA | undefined {
+        return this.readProp<CDATA>('xml:lang'); }
+    get 'translate'(): CDATA | undefined {
+        return this.readProp<CDATA>('translate'); }
+    get 'id'(): CDATA | undefined {
+        return this.readProp<CDATA>('id'); }
+    get 'conref'(): CDATA | undefined {
+        return this.readProp<CDATA>('conref'); }
+    get 'outputClass'(): CDATA | undefined {
+        return this.readProp<CDATA>('outputClass'); }
+    get 'className'(): CDATA | undefined {
+        return this.readProp<CDATA>('className'); }
+}
 
 // export interface IntProlog extends IntFilters, IntLocalization {
 //     className?: CDATA;
